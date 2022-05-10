125ae83fe237afa0dbda16f53e2caf28fffeceb5
jashoney <4479@holbertonschool.com>
2022-05-10 14:40:27 +1000

    added _pall function

---
+++ pall.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ *_pall - prints the stack line by line , or exits if empty
+ * @stack: pointer to top of stack
+ * @line_number: line number
+ * Return: void
+ */
+
+void _pall(stack_t **stack, unsigned int line_number)
+{
+
+	if ((*stack) == NULL || stack == NULL)
+		return;
+	while (*stack != NULL)
+	{
+		printf("%d\n", (*stack)->n);
+		*stack = (*stack)->n;
+	}
+}
