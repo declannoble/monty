92f1b5f4d6c47c6c3fd0aac67a44a1fd066d1262
declannoble <declanjnoble@hotmail.com>
2022-05-09 18:05:24 -0700

    build: initial commit

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * _nop - performs no operation
+ * @stack: pointer to top of stack
+ * @line_number: line number
+ * Return: void
+ */
+
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
