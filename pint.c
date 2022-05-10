9939d6bafcc9caec9f239213bf95f6742982e50a
declannoble <declanjnoble@hotmail.com>
2022-05-09 20:14:01 -0700

    build: initial commit

---
+++ pint.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ *_pint - prints value on top of stack, or exits if empty
+ * @stack: pointer to top of stack
+ * @line_number: line number
+ * Return: void
+ */
+
+void _pint(stack_t **stack, unsigned int line_number)
+{
+
+	if (*stack == NULL || stack == NULL)
+	{
+		printf("L%d: can't pint, stack empty", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
