9fd3cf2ea1f3d7075daa040332f26181cf59fc68
declannoble <declanjnoble@hotmail.com>
2022-05-09 20:49:10 -0700

    build: added prototypes

--- monty.h
+++ monty.h
@@ -35,5 +35,7 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void _pint(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
 
 #endif /* end guard monty_h */
