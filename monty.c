71ea389ecd1c8fe6852b46297c5d750dfed3fb9b
jashoney <4479@holbertonschool.com>
2022-05-10 13:01:07 +1000

    tokenising .m files

--- monty.c
+++ monty.c
@@ -9,10 +9,11 @@
 
 int main(int argc, char **argv)
 {
-	unsigned int line_number = 0, readflag = 0;
+	unsigned int line_number = 0, readflag = 0, i = 0, j = 0;
 	size_t line_buf_size = 0;
 	ssize_t line_size;
 	char *newline = NULL;
+	char *token = NULL, *token2 = NULL, *token3 = NULL;
 	FILE *fp = fopen(argv[1], "r");
 
 	if (argc != 2)
@@ -32,14 +33,25 @@ int main(int argc, char **argv)
 	{
 		line_number++;
 		printf("line[%d]: chars=%ld, buf size=%lu, contents: %s\n", line_number, line_size, line_buf_size, newline);
-/*		opcode = parse(newline);				
-		flag = function_caller(opcode, line_number);
+		token = strtok(newline, " \n\r\t");
+		if (token != NULL)
+		{
+			token2 = strtok(NULL, " \n\r\t");
+			if (token2 != NULL )
+				token3 = strtok(NULL, " \n\r\t");
+		}
+		else
+		{
+			token2 = NULL;
+			token3 = NULL;
+		}
+		printf("token = %s, token2 = %s, token3 = %s\n", token, token2, token3);
+/*		flag = function_caller(opcode, line_number); 
 		if (flag == -1)
 		{
 			fprintf("L%d: unknown instruction %s\n", line_number, opcode);
 			exit (EXIT_FAILURE);
-		}
-		free (opcode); */
+		} */
 		line_size = getline(&newline, &line_buf_size, fp);
 	}
 	fclose(fp);
