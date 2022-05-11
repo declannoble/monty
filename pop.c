d0e9ec3f2f20f85d7503f222db5f00d06509d58c
jashoney <4479@holbertonschool.com>
2022-05-11 10:20:46 +1000

    fixed error msg

--- pop.c
+++ pop.c
@@ -13,7 +13,7 @@ void _pop(stack_t **stack, unsigned int line_number)
 
 	if (stack == NULL || *stack == NULL)
 	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	current = *stack;
