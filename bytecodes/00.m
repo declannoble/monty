4ae473abfd0bc109ae9634553463d5382176c2d8
Cienna Nguyen <4326@holbertonschool.com>
2022-05-12 14:52:00 +1000

    Add conditon for neg numbers

--- bytecodes/00.m
+++ bytecodes/00.m
@@ -1,5 +1,7 @@
-push 1
+push -1
+push 0
+push -0
 push 2
-push 3
-rotr
+push 10
+push -14
 pall
--- push.c
+++ push.c
@@ -11,7 +11,7 @@ void _push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *newnode;
 	char *value;
-	int number, i = 0, flag = 0;
+	int number, i = 0, flag = 0, sign;
 
 	if (stack == NULL)
 	{
@@ -37,12 +37,19 @@ void _push(stack_t **stack, unsigned int line_number)
 		flag = isdigit(value[i]);
 		if (flag == 0)
 		{
-			fprintf(stderr, "L%d: usage: push integer\n", line_number);
-			exit(EXIT_FAILURE);
+			if (value[0] == '-' && value[1] != '0')
+				sign = 1;
+			else if (value[0] == '-' && value[1] == '0')
+				sign = 1;
+			else
+			{
+				fprintf(stderr, "L%d: usage: push integer\n", line_number);
+				exit(EXIT_FAILURE);
+			}
 		}
 		i++;
 	}
-	number = atoi(value);
+	number = atoi(value) * sign;
 	newnode->n = number;
 	newnode->prev = NULL;
 	newnode->next = *stack;
