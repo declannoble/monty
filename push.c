264c0f258af9422ad7662132cc71fad6e507b707
Cienna Nguyen <4326@holbertonschool.com>
2022-05-12 15:21:04 +1000

    Reverse to working version

--- push.c
+++ push.c
@@ -11,38 +11,38 @@ void _push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *newnode;
 	char *value;
-	int number, i, sign;
+	int number, i = 0, flag = 0;
 
 	if (stack == NULL)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+
 	newnode = malloc(sizeof(*newnode));
 	if (newnode == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
+
 	value = strtok(NULL, " \n\t\r");
 	if (value == NULL)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	for (i = 0; value[i] != '\0'; i++)
+	while (value[i] != '\0')
 	{
-		if (isdigit(value[i]) == 0 && value[0] == '-')
-			sign = 1;
-		if (isdigit(value[i]) == 0 && value[0] != '-')
+		flag = isdigit(value[i]);
+		if (flag == 0)
 		{
 			fprintf(stderr, "L%d: usage: push integer\n", line_number);
 			exit(EXIT_FAILURE);
 		}
+		i++;
 	}
-	if (value[1] == '0')
-		sign = 1;
-	number = atoi(value) * sign;
+	number = atoi(value);
 	newnode->n = number;
 	newnode->prev = NULL;
 	newnode->next = *stack;
