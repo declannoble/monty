4a861be0e4c30b838492e1a6536314ac2d242df8
Cienna Nguyen <4326@holbertonschool.com>
2022-05-12 15:19:27 +1000

    Add neg num condition

--- push.c
+++ push.c
@@ -11,38 +11,38 @@ void _push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *newnode;
 	char *value;
-	int number, i = 0, flag = 0;
+	int number, i, sign;
 
 	if (stack == NULL)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-
 	newnode = malloc(sizeof(*newnode));
 	if (newnode == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-
 	value = strtok(NULL, " \n\t\r");
 	if (value == NULL)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	while (value[i] != '\0')
+	for (i = 0; value[i] != '\0'; i++)
 	{
-		flag = isdigit(value[i]);
-		if (flag == 0)
+		if (isdigit(value[i]) == 0 && value[0] == '-')
+			sign = 1;
+		if (isdigit(value[i]) == 0 && value[0] != '-')
 		{
 			fprintf(stderr, "L%d: usage: push integer\n", line_number);
 			exit(EXIT_FAILURE);
 		}
-		i++;
 	}
-	number = atoi(value);
+	if (value[1] == '0')
+		sign = 1;
+	number = atoi(value) * sign;
 	newnode->n = number;
 	newnode->prev = NULL;
 	newnode->next = *stack;
